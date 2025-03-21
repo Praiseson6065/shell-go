3865eb1c639d80f8184c59d0c358b97ed30c2885
Praiseson <y.praiseson@outlook.com>
2025-03-21 09:08:12 +0530

    echo cmd

--- app/main.go
+++ app/main.go
@@ -4,25 +4,51 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+func invalidCommand(command string) {
+	fmt.Printf("%s: command not found\n", command)
+}
+
+func handler(command string) {
+	var args = strings.Split(command, " ")
+	switch args[0] {
+	case "exit":
+		if len(args) > 1 {
+			exitCode, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Invalid exit code")
+				return
+			}
+			os.Exit(exitCode)
+		}
+		return
+	case "echo":
+		fmt.Println(strings.Join(args[1:], " "))
+	default:
+		invalidCommand((command))
+	}
+
+}
+
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
 
-	// Wait for user input
-	cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Wait for user input
+		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd = strings.TrimSuffix(cmd, "\n")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		handler(cmd)
 	}
-	if cmd[:len(cmd)-1] == "exit 0" {
-		os.Exit(0)
-	}
-	fmt.Println(cmd[:len(cmd)-1] + ": command not found")
-	main()
+
 }
